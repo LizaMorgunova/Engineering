088f5066b5d6581b7ad6732b25e48673779bf5c5
LizaMorgunova <lizmorden05@gmail.com>
2025-10-17 15:20:16 +0500

    Create lab5.py

---
+++ lab5.py
@@ -0,0 +1,5 @@
+user_input = input("Введите последовательность чисел: ")
+numbers_list = user_input.split()
+numbers_tuple = tuple(numbers_list)
+print("Список:", numbers_list)
+print("Кортеж:", numbers_tuple)
